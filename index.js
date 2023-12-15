105971912e0a780fc14e2ba7efcd1cdaacb75484
rezha4 <rezha.bahari@gmail.com>
2023-12-15 10:43:02 +0700

    added projects, tech stack

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ expandButton.addEventListener("click", () => {
     longStory.removeAttribute("hidden");
   }
 });
+
+// const projects = [
+//   {
+//     title: "title",
+//     img: "./images/messagingApp.webp",
+//     description: "description",
+//     techBadge: ["node", "react"],
+//   },
+// ];
+
+// const projectCardContainer = document.querySelector("card-container");
+
+// const projectCard = document.createElement("div");
+// projectCard.classList.add("card");
+// projectCard.classList.add("subtle-shadow");
+// const projectTitle = document.createElement("h3");
+// const projectImg = document.createElement("div");
+// const projectDescripiton = document.createElement("p");
+// const projectBadges = document.createElement("div");
+
+// projects.forEach(project => {
+//     projectTitle.textContent = project.title;
+
+    
+//     projectCardContainer.appendChild(projectCard);
+
+// })
