567f386e89beb0d0390ec795dd9a82ebd0006df4
rezha4 <rezha.bahari@gmail.com>
2023-12-25 09:30:24 +0700

    exported auth

--- firebased/src/App.jsx
+++ firebased/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  console.log(auth.currentUser);
+  
   const signIn = async () => {
     await createUserWithEmailAndPassword(auth, email, password);
   }
